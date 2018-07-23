1d4f15515db47fa7b3c9faa1e323750f5b7af881
Serge Da Silva <serge.dasilva@avisto.com>
2018-07-23 12:03:12 +0200

    jwt save user

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const validator = require('validator'); 
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 var Schema = mongoose.Schema
 
-var userSchema = new Schema ({
+var UserSchema = new Schema ({
     email: {
         type: String,
         required: true,
@@ -31,7 +33,26 @@ var userSchema = new Schema ({
     }]
 });
 
-var User = mongoose.model('User', userSchema);
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();  
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access: access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access: access, token: token}]);
+
+    return user.save().then(() => {
+        return token;
+    })
+}
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {
     User
--- server/server.js
+++ server/server.js
@@ -100,8 +100,10 @@ app.post('/users', (req, res) => {
     
     var user = new User(body)
 
-    user.save().then((user) => {
-        res.send(user);
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
     });
