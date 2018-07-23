721a853b75e0a1648daa1e5228a75aa948eeade3
Serge Da Silva <serge.dasilva@avisto.com>
2018-07-23 15:48:42 +0200

    Private routes

--- server/models/user.js
+++ server/models/user.js
@@ -35,7 +35,7 @@ var UserSchema = new Schema ({
 
 UserSchema.methods.toJSON = function () {
   var user = this;
-  var userObject = user.toObject();  
+  var userObject = user.toObject();
 
   return _.pick(userObject, ['_id', 'email']);
 };
@@ -49,6 +49,23 @@ UserSchema.methods.generateAuthToken = function () {
 
     return user.save().then(() => {
         return token;
+    })
+}
+
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     })
 }
 
