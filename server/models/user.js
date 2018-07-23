6ed81aa1645ed2337785f444e3d8049b423e985b
Serge Da Silva <serge.dasilva@avisto.com>
2018-07-23 16:30:22 +0200

    hash password

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator'); 
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 var Schema = mongoose.Schema
 
 var UserSchema = new Schema ({
@@ -32,6 +33,21 @@ var UserSchema = new Schema ({
         }
     }]
 });
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+})
 
 UserSchema.methods.toJSON = function () {
   var user = this;
